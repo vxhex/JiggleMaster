6bf2014710e50398d4e28bc1a72adb06c032ccc1
Andy Giannini <tabete@gmail.com>
2016-03-05 15:12:33 -0600

    Make some noise

--- app/src/main/java/com/vxhex/jigglemaster/MainActivity.java
+++ app/src/main/java/com/vxhex/jigglemaster/MainActivity.java
@@ -5,6 +5,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
     private final float JIGGLE_DELTA = 13;
 
-    private int numberOfJiggles = 0;
+    private int _numberOfJiggles = 0;
 
     private SensorManager _jiggleManager;
     private float _accelerationWithoutGravity;
     private float _accelerationCurrentWithGravity;
     private float _accelerationLastWithGravity;
+
+    private SoundPool _soundPool;
+    private int soundID;
+    boolean loaded = false;
 
     private final String[] _healthyBodyHealthyMind = {
             "Way to jiggle it, cadet!",
@@ -41,17 +48,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        // Initialize the sensor manager
-        _jiggleManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        _jiggleManager.registerListener(mSensorListener,
-                                        _jiggleManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-                                        SensorManager.SENSOR_DELAY_NORMAL);
-
-        // Initialize accelerations
-        _accelerationWithoutGravity = 0.00f;
-        _accelerationCurrentWithGravity = SensorManager.GRAVITY_EARTH;
-        _accelerationLastWithGravity = SensorManager.GRAVITY_EARTH;
+        initializeJiggle();
+        initializeSounds();
     }
 
     @Override
@@ -68,12 +66,47 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    private void initializeJiggle() {
+        // Initialize the sensor manager
+        _jiggleManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        _jiggleManager.registerListener(mSensorListener,
+                _jiggleManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Initialize accelerations
+        _accelerationWithoutGravity = 0.00f;
+        _accelerationCurrentWithGravity = SensorManager.GRAVITY_EARTH;
+        _accelerationLastWithGravity = SensorManager.GRAVITY_EARTH;
+    }
+
+    private void initializeSounds() {
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        _soundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+        _soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded = true;
+            }
+        });
+        soundID = _soundPool.load(this, R.raw.sound1, 1);
+    }
+
     private void jiggleUpdate() {
-        numberOfJiggles++;
-        if (numberOfJiggles % 5 == 0) {
+        _numberOfJiggles++;
+        if (_numberOfJiggles % 3 == 0) {
+            // Set a random encouragement
             int random = new Random().nextInt(_healthyBodyHealthyMind.length);
             TextView text = (TextView)findViewById(R.id.textView1);
             text.setText(_healthyBodyHealthyMind[random]);
+
+            // Play a random noise
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            float volume = actualVolume / maxVolume;
+            if (loaded) {
+                _soundPool.play(soundID, volume, volume, 1, 0, 1f);
+            }
         }
     }
 
@@ -92,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
 
             if (_accelerationWithoutGravity > JIGGLE_DELTA) {
                 jiggleUpdate();
-                Log.d("JiggleMaster", "Way to jiggle it, cadet! " + numberOfJiggles);
+                Log.d("JiggleMaster", "Way to jiggle it, cadet! " + _numberOfJiggles);
             }
         }
 
